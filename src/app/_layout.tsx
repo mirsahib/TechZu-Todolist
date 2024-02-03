f2963683297f389095cef4c75857ad143b41bac3
Mir Sahib <mirsahib24@gmail.com>
2024-02-03 17:40:26 +0600

    :hammer: Add setting screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,55 @@
-import { Stack } from 'expo-router';
-import { Pressable, Text } from 'react-native';
+import { Link, Stack } from 'expo-router';
+import { Pressable, Text, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
+import { useRef, useState } from 'react';
+import Popover from 'react-native-popover-view';
 
 export default function HomeLayout() {
+  const touchable = useRef(null);
+  const [showPopover, setShowPopover] = useState(false);
+
+  const renderPopoverContent = () => (
+    <View style={{ padding: 16 }}>
+      <Link href={'/setting'} onPress={() => setShowPopover(false)}>
+        <Text>Settings</Text>
+      </Link>
+      {/* Add your popover content here */}
+    </View>
+  );
+
   return (
-    <Stack
-      screenOptions={{
-        headerTitle: 'Home',
-        headerRight: () => (
-          <Pressable>
-            <Entypo name="dots-three-vertical" size={24} color="black" />
-          </Pressable>
-        ),
-        headerShown: true,
-        headerStyle: {
-          // backgroundColor: 'blue',
-        },
-        headerTintColor: 'white',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    />
+    <>
+      <Stack
+        screenOptions={{
+          headerTitle: 'Home',
+          headerRight: () => (
+            <Pressable
+              ref={touchable}
+              onPress={() => {
+                console.log('show popover');
+                setShowPopover(true);
+              }}
+            >
+              <Entypo name="dots-three-vertical" size={24} color="black" />
+            </Pressable>
+          ),
+          headerShown: true,
+          headerStyle: {
+            // backgroundColor: 'blue',
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+      <Popover
+        from={touchable}
+        isVisible={showPopover}
+        onRequestClose={() => setShowPopover(false)}
+      >
+        {renderPopoverContent()}
+      </Popover>
+    </>
   );
 }
--- src/app/setting.tsx
+++ src/app/setting.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { Button } from 'react-native';
 
 const Setting = () => {
   return (
-    <View>
-      <Text>Setting</Text>
+    <View style={styles.container}>
+      <View>
+        {/* Display user information */}
+        {/* <Image source={{ uri: '' }} style={styles.profileImage} /> */}
+        <View style={styles.taskCard}>
+          <Text style={styles.userInfoText}>{`Name: `}</Text>
+        </View>
+        <View style={styles.taskCard}>
+          <Text style={styles.userInfoText}>{`Email: `}</Text>
+        </View>
+        {/* Logout button */}
+        <Button title="Logout" onPress={() => {}} />
+      </View>
     </View>
   );
 };
 
 export default Setting;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 16,
+  },
+  taskCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    padding: 16,
+    marginVertical: 8,
+    marginBottom: 16,
+    borderRadius: 8,
+    elevation: 2, // Add elevation for shadow on Android
+    width: '100%', // Make the card full width
+  },
+  userInfoText: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+});
